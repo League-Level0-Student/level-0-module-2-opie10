cfb3375d6e90a6911696b0662f0823c04e06579b
Tacoalarmclock <45108867+Tacoalarmclock@users.noreply.github.com>
2019-02-11 18:26:18 -0800

    Amazing week 5
    finished nasa countdown almost done soundtrack.

--- src/extra/NasaCountdown.java
+++ src/extra/NasaCountdown.java
@@ -3,7 +3,9 @@
 
 package extra;
 
- /* NASA wants to shoot a rocket into orbit. 
+import javax.swing.JOptionPane;
+
+/* NASA wants to shoot a rocket into orbit. 
   * Somebody has to do the countdown – or the rocket won’t launch.
  * 
  * The ground control chief will tell you where to start – then count down to 0. 
@@ -14,26 +16,34 @@ package extra;
 
 public class NasaCountdown {
 	public static void main(String[] args) throws InterruptedException {
-		// 2. Use a pop-up to ask the user where to start counting from
 
+		// 2. Use a pop-up to ask the user where to start counting from
+	String  ans=	JOptionPane.showInputDialog("What would you like to count down from?");
 		// 3. Change the countdown to use the new starting point
 		
 		// 1. Print a countdown from 10 to 0 on the console
-	
+		int ans2 =Integer.parseInt(ans);
+	for (int i = ans2; i > -1; i--) {
+			System.out.println(i);
+
 			// 4. Use the speak method to hear the countdown.
-		
-			// 6. Use the following code to make the program wait one second for each number: Thread.sleep(1000);
-		
-		// 5. when the counting is done, speak "blastoff!"
+			speak(""+i );
+			if (i==0) {
+				speak("BLASTOFF!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+				
+			}
+			// 6. Use the following code to make the program wait one second for each
+			// number: Thread.sleep(1000);
+			Thread.sleep(1000);
+			// 5. when the counting is done, speak "blastoff!"
+		}
 	}
 
-static void speak(String words) {
-   	 try {
-   		 Runtime.getRuntime().exec("say " + words).waitFor();
-   	 } catch (Exception e) {
-   		 e.printStackTrace();
-   	 }
-  }
+	static void speak(String words) {
+		try {
+			Runtime.getRuntime().exec("say " + words).waitFor();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
-
-
--- src/extra/SoundtrackToLife.java
+++ src/extra/SoundtrackToLife.java
@@ -11,13 +11,21 @@ public class SoundtrackToLife {
 	public static void main(String[] args) {
 
 		// 1. Adjust this pop-up to find out what mood the user is in.
-		int userMood = JOptionPane.showOptionDialog(null, "The question", "Pop-up Title", 0,
-				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Mood1", "Mood2", "Mood3" }, null);
+		int userMood = JOptionPane.showOptionDialog(null, "What are you feeling like?", "Pop-up Title", 0,
+				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Happy ", "Sad ", "Angry","Stressed" }, null);
 		
 		// 2. Their answer is stored in the userMood variable. Print it out.
-
+System.out.println(userMood);
 		// 3. If they are in a stressed mood, use the playVideo method to play a calming song from YouTube.
-		
+		if (userMood==0) {playVideo("https://www.youtube.com/watch?v=y6Sxv-sUYtM");
+			
+		}
+		else if (userMood==1) {playVideo("https://www.youtube.com/watch?v=hLQl3WQQoQ0");
+			
+		}
+		else if (userMood ==2) {playVideo("https://www.youtube.com/watch?v=-tJYN-eG1zk");
+			
+		}
 
 		
 		// 4. Play different songs for other moods.
@@ -42,6 +50,3 @@ public class SoundtrackToLife {
 		}
 	}
 }
-
-
-
