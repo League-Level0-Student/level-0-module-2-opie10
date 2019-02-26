9d62bba1cabbd19c0f752ceeacc7321b527f5807
Tacoalarmclock <45108867+Tacoalarmclock@users.noreply.github.com>
2019-02-25 18:28:13 -0800

    week 6
    fun animal farm done doing magic 8 ball

--- src/methods/AnimalFarm.java
+++ src/methods/AnimalFarm.java
@@ -1,11 +1,11 @@
 package methods;
 
 import java.applet.AudioClip;
+import java.net.URI;
 
 //Copyright (c) The League of Amazing Programmers 2013-2017
 
 //Level 0
-
 
 import javax.swing.JApplet;
 import javax.swing.JOptionPane;
@@ -16,8 +16,48 @@ public class AnimalFarm {
 		/*
 		 * 1. Ask the user which animal they want, then play the sound of that animal.
 		 */
-		
-		/* 2. Make it so that the user can keep entering new animals. */
+
+		String amount = JOptionPane.showInputDialog("How many times would you like to listen to animal sounds?");
+		int amountint = Integer.parseInt(amount);
+		for (int i = 0; i < amountint; i++) {
+			String usera = JOptionPane.showInputDialog("What animal would you like to listen to? you have "
+					+ (amountint - i) + " sounds left \n Disclamer: we are not liable if you are annoyed");
+			/* 2. Make it so that the user can keep entering new animals. */
+
+			if (usera.equals("cow")) {
+				playMoo();
+
+			}
+
+			else if (usera.equals("duck")) {
+				playQuack();
+
+			} else if (usera.equals("dog")) {
+				playWoof();
+
+			} else if (usera.equals("llama")) {
+				playLlama();
+
+			} else if (usera.equals("cat")) {
+				playCat();
+
+			} else {
+				JOptionPane.showMessageDialog(null,"Your answer had no file");
+
+				playVideo("http://weebls-stuff.com/toons/badgers/");
+			}
+
+		}
+
+	}
+
+	static void playVideo(String youTubeLink) {
+		try {
+			URI uri = new URI(youTubeLink);
+			java.awt.Desktop.getDesktop().browse(uri);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	void playMoo() {
@@ -30,6 +70,14 @@ public class AnimalFarm {
 
 	void playWoof() {
 		playNoise(woofFile);
+	}
+
+	void playLlama() {
+		playNoise(llamaFile);
+	}
+
+	void playCat() {
+		playNoise(meowFile);
 	}
 
 	String quackFile = "quack.wav";
--- src/random/Magic8Ball.java
+++ src/random/Magic8Ball.java
@@ -1,35 +1,49 @@
 //    Copyright (c) The League of Amazing Programmers 2013-2018
 //    Level 0
 
-
 package random;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class Magic8Ball {
+	public static void main(String[] args) {
 
-	// 1. Make a main method that includes all the steps below….
+		// 1. Make a main method that includes all the steps below….done
 
-	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
+		// 2. Make a variable that will hold a random number and put a random number
+		// into this variable using "new Random().nextInt(4)"
+		int rand = new Random().nextInt(5);
+		// 3. Print out this variable
+		System.out.println(rand);
+		// 4. Get the user to enter a question for the 8 ball
+		JOptionPane.showInputDialog("What is your question for the all knowing magic 8 ball?");
+		// 5. If the random number is 0
+		if (rand == 2) {
+			JOptionPane.showMessageDialog(null, "Yes");
+		}
+		// -- tell the user "Yes"
 
-	// 3. Print out this variable
+		// 6. If the random number is 1
+		else if (rand == 1) {
+			JOptionPane.showMessageDialog(null, "No");
+		}
+		// -- tell the user "No"
 
-	// 4. Get the user to enter a question for the 8 ball
+		// 7. If the random number is 2
+		else if (rand == 0) {
+			JOptionPane.showMessageDialog(null, "I am tired today, you should talk to my assistant,Google.");
+		}
+		// -- tell the user "Maybe you should ask Google?"
 
-	// 5. If the random number is 0
-
-	// -- tell the user "Yes"
-
-	// 6. If the random number is 1
-
-	// -- tell the user "No"
-
-	// 7. If the random number is 2
-
-	// -- tell the user "Maybe you should ask Google?"
-
-	// 8. If the random number is 3
-
-	// -- write your own answer
-
+		// 8. If the random number is 3
+		else if (rand == 4) {
+			JOptionPane.showMessageDialog(null, "Your question is not revealed to me at the moment");
+		}
+		else if (rand == 3) {
+			JOptionPane.showMessageDialog(null, "Maybe,maybe not,I will not tell you");
+		// -- write your own answer
+	}
+}
 }
