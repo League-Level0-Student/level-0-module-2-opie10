63c3ca2c16b508fcddd119e72decf184bdf9f8ec
Hausen <45108867+Tacoalarmclock@users.noreply.github.com>
2019-03-04 18:29:31 -0800

    Week8
    most things done, started on processing,done all random,extra and methods.

---
+++ src/Dot_race/Dot_race.pde
@@ -0,0 +1,19 @@
+//Make a variable to hold the X co-ordinate of the dot and set it to void setup() {
+   
+ 
+void setup() {
+ size(800, 200);}
+void draw() {
+    //make it a nice color
+fill(#18EDE8);
+noStroke();
+    //if the mouse is pressed...
+
+    //... change the X co-ordinate so that the dot moves to the right
+    //Draw an ellipse of height and width 10Make sure to use your variable for the X position.
+    ellipse(20,20,20,20);
+    //Make your dot move really fast so that it can win the race 
+    
+        //(you have to figure out what part of your code to change)
+    //Use this method to play a ding when your dot crosses the finish line. 
+}
--- src/random/Validation.java
+++ src/random/Validation.java
@@ -9,17 +9,45 @@ import javax.swing.JOptionPane;
 
 public class Validation {
 	public static void main(String[] args) {
+
 		
-		Random randomMaker = new Random();
+
 		
-		int randomNumber = randomMaker.nextInt(5);
+
 		
-		System.out.println(randomNumber);
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
+		for (int i = 0; i < 11; i++) {
+			Random randomMaker = new Random();
+			int rN = randomMaker.nextInt(5);
+			System.out.println(rN);
+			if (rN == 0) {
 
+				JOptionPane.showMessageDialog(null, "You are looking good today");
+
+			}
+			else if (rN == 1) {
+
+				JOptionPane.showMessageDialog(null, "Your clothes are nice");
+
+			}
+
+			else if (rN == 2) {
+
+				JOptionPane.showMessageDialog(null, "You are really kind");
+
+			} else if (rN == 3) {
+
+				JOptionPane.showMessageDialog(null, "You are good at coding");
+
+			} else if (rN == 4) {
+
+				JOptionPane.showMessageDialog(null, "You are smart ");
+
+			}
+		}
 		// 2. Repeat all the code above 10 times
-		
+
 		// 3. Find someone to test out your program. They will like it :)
 	}
 }
